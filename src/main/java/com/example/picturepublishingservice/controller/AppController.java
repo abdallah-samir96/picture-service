4cbc8af1c1623ce575d0be11651f224489aa6c30
Abdallah Samir <asamir@coral.io>
2023-06-17 18:15:16 +0300

    enhancements.

--- src/main/java/com/example/picturepublishingservice/controller/AppController.java
+++ src/main/java/com/example/picturepublishingservice/controller/AppController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+ @RestController
 public class AppController {
 
     @Value("${app.name}")
