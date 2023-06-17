a61c34e5336c709d433f7724f96031660d26ddad
Abdallah Samir <asamir@coral.io>
2023-06-17 18:14:22 +0300

    adding the structure of the project.

---
+++ src/main/java/com/example/picturepublishingservice/controller/AppController.java
@@ -0,0 +1,42 @@
+package com.example.picturepublishingservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class AppController {
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value("${app.author}")
+    private String appAuthor;
+
+    @Value("${app.mentor}")
+    private String appMentor;
+
+    @GetMapping("/info")
+    public ResponseEntity<Map<String, String>> getAppInfo(){
+
+        System.out.println("getting App Info " + Thread.currentThread().getName());
+        var appInfo = Map
+                .of(
+                        "name", this.appName,
+                        "version", this.appVersion,
+                        "author", this.appAuthor,
+                        "mentor", this.appMentor
+                );
+
+       return ResponseEntity.status(HttpStatus.OK).body(appInfo);
+
+    }
+}
